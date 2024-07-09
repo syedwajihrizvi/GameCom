8ea653ba64a44cc05c81159c8e04e9e240b37cf7
Wajih Rizvi <swh2rizv@edu.uwaterloo.ca>
2024-07-08 23:51:12 -0400

    Fixed home page layout issues

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,44 +1,63 @@
+import { Box, Button, Center, Image, Input, Menu, MenuButton, MenuItem, MenuList, Text } from "@chakra-ui/react"
+import { HStack, Heading, VStack } from "@chakra-ui/react"
+import sectionOneImage from "../assets/image-1.png"
+import sectionTwoImage from "../assets/image-2.png"
 import { ChevronRightIcon, ChevronDownIcon } from "@chakra-ui/icons"
-import { Box, Button, Flex, HStack, Heading, Input, Menu, MenuButton, MenuItem, MenuList, Spacer, Text, VStack } from "@chakra-ui/react"
-
 function Main() {
     return (
-        <VStack>
-            <Flex className="background" w="100%">
-                <Flex w="100%">
-                    <Box paddingLeft={40} paddingTop={10}>
-                        <Heading>Title</Heading>
-                    </Box>
-                    <Spacer />
-                    <Box paddingRight={40} padding={10}>
-                        <HStack>
-                            <Menu>
-                                <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                                    Language
-                                </MenuButton>
-                                <MenuList>
-                                    <MenuItem>English</MenuItem>
-                                    <MenuItem>French</MenuItem>
-                                </MenuList>
-                            </Menu>
-                            <Button backgroundColor='red' color='white'>Sign In</Button>
-                        </HStack>
-                    </Box>
-                </Flex>
-                <Flex className="main-text" minWidth='max-content' alignItems='center'>
+        <VStack backgroundColor='black'>
+            <Box className="background" w='100%'>
+                <Box className="logo">
+                    <Heading>TITLE</Heading>
+                </Box>
+                <Box className="header-buttons">
+                    <HStack>
+                        <Menu>
+                            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} color='white' backgroundColor='rgba(22, 0, 0, 0.2)'>
+                                Language
+                            </MenuButton>
+                            <MenuList>
+                                <MenuItem>English</MenuItem>
+                                <MenuItem>French</MenuItem>
+                            </MenuList>
+                        </Menu>
+                        <Button backgroundColor='red' color='white'>
+                            Sign In
+                        </Button>
+                    </HStack>
+                </Box>
+                <Center className="register-main">
                     <VStack>
-                        <Heading as='h1' size='3xl' fontFamily="Netflix Sans">Your Favorite Games, in your favorite place</Heading>
-                        <Heading as='h5' size='lg' paddingTop={3} fontFamily="Netflix Sans">Play Anywhere. Cancel Anytime.</Heading>
-                        <Heading as='h4' size='sm' paddingTop={3} paddingBottom={5} fontFamily="Netflix Sans">Ready to play? Enter your email to create or restart your membership.</Heading>
+                        <Heading color="white" as='h2' size='3xl'>Unlimited Games, streams, and more.</Heading>
+                        <Heading color="white" as='h4' size='lg'>Play Anywhere. Cancel Anytime</Heading>
+                        <Text color="white" as="b" fontSize='lg'>Ready to watch? Enter your email to create or restart your membership.</Text>
                         <HStack>
-                            <Input placeholder='Email Adress' height='50px' width='360px' borderRadius={3} backgroundColor="rgba(0, 0, 0, 0.4)"/>
-                            <Button rightIcon={<ChevronRightIcon boxSize={8} color='white'/>} backgroundColor='red' width="220px" height="50px" borderRadius={3}>
-                                <Text fontSize='2xl' color='white'>Get Started</Text>
+                            <Input placeholder='Enter Email Address' height={55} borderRadius={1} backgroundColor='rgba(22, 0, 0, 0.2)'/>
+                            <Button backgroundColor='red' color="white" width={340} height={55} borderRadius={2} rightIcon={<ChevronRightIcon boxSize={8}/>}>
+                                <Text as='b' fontSize='2xl'>Get Started</Text>
                             </Button>
                         </HStack>
                     </VStack>
-                </Flex>
-            </Flex>       
+                </Center>
+            </Box>
+            <Center backgroundColor='black' w='100%' paddingTop={20}>
+                <HStack>
+                    <VStack color='white'>
+                        <Heading as='h1' size='2xl'>Enjoy Anywhere. Enjoy Anytime</Heading>
+                        <Heading as='h5' size='lg'>Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</Heading>
+                    </VStack>
+                    <Image src={sectionOneImage} height={300}/>
+                </HStack>
+            </Center>   
+            <Center backgroundColor='black' w='100%' paddingTop={20}>
+                <HStack spacing={60}>
+                <Image src={sectionTwoImage} height={350}/>
+                    <VStack color='white'>
+                        <Heading as='h1' size='2xl'>Play Anywhere.</Heading>
+                        <Heading as='h5' size='lg'>Stream your favorite games anywhere you want. View our current plants.</Heading>
+                    </VStack>
+                </HStack>
+            </Center>  
         </VStack>
     )
 }
