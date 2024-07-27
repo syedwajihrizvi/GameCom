34e97f6c18fbaf89f324233992f5c9c0067fea4a
Wajih Rizvi <swh2rizv@edu.uwaterloo.ca>
2024-07-27 18:06:59 -0400

    Added heroku url

--- src/utils/services/userService.ts
+++ src/utils/services/userService.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
-
-const url = "http://localhost:3000/gamecom/api/users"
+const url = "https://protected-chamber-12422-c8b311f333a4.herokuapp.com/gamecom/api/users"
 const client = axios.create({
     baseURL: url
 })
