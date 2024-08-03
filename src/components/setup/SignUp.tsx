15f2968f1e882dc7544ef04cc755ec8eb013d258
Wajih Rizvi <swh2rizv@edu.uwaterloo.ca>
2024-08-03 18:59:02 -0400

    Fixed overflow issue

--- src/components/setup/SignUp.tsx
+++ src/components/setup/SignUp.tsx
@@ -11,17 +11,17 @@ function SignUp() {
 
     return (
         <>
-        <Text color="white" as="b" fontSize='lg'>Ready to watch? Enter your email to create or restart your membership.</Text>
-        <SimpleGrid columns={{lg:2, md:1}} spacingX={10} spacingY={2}>
-            <GridItem>
-                <Input placeholder='Enter Email Address' height={55} borderRadius={1} backgroundColor='rgba(22, 0, 0, 0.2)'  _placeholder={{ color: 'white' }}/>
-            </GridItem>
-            <GridItem>
-                <Button backgroundColor='red' color="white" width={340} height={55} borderRadius={2} rightIcon={<ChevronRightIcon boxSize={8}/>} onClick={handleGetStarted}>
-                    <Text as='b' fontSize='2xl'>Get Started</Text>
-                </Button>
-            </GridItem>
-        </SimpleGrid>
+            <Text color="white" as="b" fontSize='lg'>Ready to watch? Enter your email to create or restart your membership.</Text>
+            <SimpleGrid columns={{lg:2, md:1}} spacingX={10} spacingY={2}>
+                <GridItem>
+                    <Input placeholder='Enter Email Address' height={55} borderRadius={1} backgroundColor='rgba(22, 0, 0, 0.2)'  _placeholder={{ color: 'white' }}/>
+                </GridItem>
+                <GridItem>
+                    <Button backgroundColor='red' color="white" width={340} height={55} borderRadius={2} rightIcon={<ChevronRightIcon boxSize={8}/>} onClick={handleGetStarted}>
+                        <Text as='b' fontSize='2xl'>Get Started</Text>
+                    </Button>
+                </GridItem>
+            </SimpleGrid>
         </>
     )
 }
